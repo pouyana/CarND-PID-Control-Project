8eb6c1813581f7d818adf9de0445952bf43c381a
Pouyan Azari <pouyan.azari@uni-wuerzburg.de>
2019-04-15 12:40:23 +0200

    added some new code for twiddle and pid

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,30 +1,90 @@
 #include "PID.h"
 
-/**
- * TODO: Complete the PID class. You may add any additional desired functions.
- */
-
 PID::PID() {}
 
 PID::~PID() {}
 
-void PID::Init(double Kp_, double Ki_, double Kd_) {
-  /**
-   * TODO: Initialize PID coefficients (and errors, if needed)
-   */
-
+void PID::Init(double Kp_, double Ki_, double Kd_, bool twiddle_mode_)
+{
+  this->Kp = Kp_;
+  this->Ki = Ki_;
+  this->Kd = Kd_;
+  this->p_error = 0;
+  this->i_error = 0;
+  this->d_error = 0;
+  this->di = 0;
+  this->dp = 0;
+  this->dd = 0;
+  this->best_error = 10000000000;
+  this->twiddle_mode = twiddle_mode_;
+  this->it = 0;
+  this->change_direction = false;
 }
 
-void PID::UpdateError(double cte) {
-  /**
-   * TODO: Update PID errors based on cte.
-   */
-
+void PID::UpdateError(double cte)
+{
+  this->p_error = cte;
+  this->i_error += cte;
+  this->d_error = cte - this->p_error;
 }
 
-double PID::TotalError() {
-  /**
-   * TODO: Calculate and return the total error
-   */
-  return 0.0;  // TODO: Add your total error calc here!
+void PID::Twiddle(double tol, double cte)
+{
+  if (this->twiddle_mode == true)
+  {
+    double dsum = this->dp + this->dd + this->di;
+    while (dsum > tol)
+    {
+      this->p_error += this->dp;
+      this->i_error += this->di;
+      this->d_error += this->dd;
+      if (cte < this->best_error && this->change_direction == false)
+      {
+        this->best_error = cte;
+        this->p_error *= 1.1;
+        this->i_error *= 1.1;
+        this->d_error *= 1.1;
+      }
+      else
+      {
+        this->change_direction == true;
+        this->p_error -= 2 * this->dp;
+        this->i_error -= 2 * this->di;
+        this->d_error -= 2 * this->dd;
+        if (this->change_direction == true)
+        {
+          if (cte < this->best_error)
+          {
+            this->best_error = cte;
+            this->p_error *= 1.05;
+            this->i_error *= 1.05;
+            this->d_error *= 1.05;
+          }
+          else
+          {
+            this->p_error += this->dp;
+            this->i_error += this->di;
+            this->d_error += this->dd;
+            this->p_error *= 0.95;
+            this->i_error *= 0.95;
+            this->d_error *= 0.95;
+          }
+          this->change_direction = false;
+        }
+      }
+    }
+  }
+}
+
+void PID::Reset()
+{
+  this->p_error = 0.0;
+  this->d_error = 0.0;
+  this->i_error = 0.0;
+}
+
+double PID::TotalError()
+{
+  double steering = this->p_error * this->Kp + this->i_error * this->Ki + this->d_error * this->Kd;
+  return steering;
 }
--- src/PID.h
+++ src/PID.h
@@ -1,8 +1,11 @@
 #ifndef PID_H
 #define PID_H
 
-class PID {
- public:
+#include <vector>
+
+class PID
+{
+public:
   /**
    * Constructor
    */
@@ -17,7 +20,7 @@ class PID {
    * Initialize PID.
    * @param (Kp_, Ki_, Kd_) The initial PID coefficients
    */
-  void Init(double Kp_, double Ki_, double Kd_);
+  void Init(double Kp_, double Ki_, double Kd_, bool twiddle_mode);
 
   /**
    * Update the PID error variables given cross track error.
@@ -26,12 +29,24 @@ class PID {
   void UpdateError(double cte);
 
   /**
+   * Twiddle function which is used to optimize the Kp/Ki/Kd
+   * @param double tol
+   */
+  void Twiddle(double tol, double cte);
+
+  /**
    * Calculate the total PID error.
    * @output The total PID error
    */
   double TotalError();
 
- private:
+/**
+ * Resets the PID controller
+ * 
+ */
+  void Reset();
+
+private:
   /**
    * PID Errors
    */
@@ -41,10 +56,21 @@ class PID {
 
   /**
    * PID Coefficients
-   */ 
+   */
   double Kp;
   double Ki;
   double Kd;
+
+  /**
+   * 
+   */
+  bool twiddle_mode;
+  double dp;
+  double di;
+  double dd;
+  int it;
+  double best_error;
+  bool change_direction;
 };
 
-#endif  // PID_H
+#endif // PID_H
